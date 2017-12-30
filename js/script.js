f76f4897a55a8d81be4f72e313dc26166623e172
Matteo <gand988@gmail.com>
2017-12-30 01:07:35 +0100

    Add files via upload
    new images;
    better buttons;
    save button now work

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-  // function that search CHECKED checkbox
   // array with the character images
   var imagesCharacter = ["img/crono.png","img/marle.png","img/lucca.png","img/frog.png","img/robo.png","img/ayla.png","img/magus.png"];
-  // script for myCanvas
+
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
+
+  // drawing function
   function drawingSelected() {
+    // clear the canvas if the user press 'Generate'
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, 444, 125);
     var selected = [];
     // check for any input:checked checkbox and push the VALUE into the array
     $('.charSelect input:checked').each(function() {
       selected.push($(this).attr('value'));
     });
 
-    // search image and draw
-    canvas.setAttribute('crossOrigin','anonymous');
-
+    // search image inside the imagesCharacter[] and draw the image
     for (var i = 0; i < selected.length; i++) {
       var image = new Image(148,125);
       image.src = imagesCharacter[selected[i]];
-      image.setAttribute('crossOrigin', 'anonymous'); // works for me
+      // image.setAttribute('crossOrigin', 'anonymous'); // maybe for
       image.onload = function() {
-          context.drawImage(this, 0, 0);
-          context.translate(148, 0);
+        context.drawImage(this, 0, 0);
+        context.translate(148, 0);
       }
     }
+    $('.CTchara').prop("checked", false);   // set checkbox :uncheck
   }
 
-  var link = document.createElement('a');
-      link.innerHTML = 'download image';
+  function uncheckBox(){
+    $('.CTchara').prop("checked", false);
+    context.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
+    context.clearRect(0, 0, 444, 125);      // clear the canvas
+  }
+
+  // SAVE CODE
+  // var link = document.createElement('a');
+  const link = document.querySelector('#saveImage');
+      // link.innerHTML = 'download image';
       link.addEventListener('click', function(ev) {
-        // link.setAttribute('crossOrigin', 'anonymous'); // works for me
       link.href = canvas.toDataURL();
-      link.download = "mypainting.png";
+      link.download = "myCharacters.png";
   }, false);
-  document.body.appendChild(link);
+  // document.body.appendChild(link);
