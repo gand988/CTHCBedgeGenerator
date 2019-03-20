400c402eb571dedfdbdefb09ebc5af16f97d2997
gand988 <gand988@gmail.com>
2019-03-20 20:01:15 +0100

    ##Ver 2.5 ###Functionality * Name list don't reset after generate the badge. Change one or more characters without have the list reset. * Reset button clear all. ###Coding * removed jQuery * switch from jquery to JS (because better knowledge). * cleaner JS code

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 This website as the porpuse to create faster and easy a badge to put in the streaming while playing Chrono trigger for the [#CTHC](http://fourjobfiesta.com/cthc/)
 
 ### Version 2.0
-* new design to match an ingame menu
-* three badge of choise 
-  * character sprite + thumb
-  * only thumb
-  * only character sprite
+* new design to match the ingame menu
+* three badge of choice
+  * character body + face
+  * only face
+  * only character body
   
+##Ver 2.5
+###Functionality
+* Name list don't reset after generate the badge. Change one or more characters without have the list reset.
+* Reset button clear all. 
+###Coding
+* removed jQuery
+* switch from jquery to JS (because better knowledge).
+* cleaner JS code
--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
   // array with the character images
-  var imagesCharacter = ["img/crono.png","img/marle.png","img/lucca.png","img/frog.png","img/robo.png","img/ayla.png","img/magus.png"];
-  var imagesThumbCharacter = ["img/thumb_crono.png","img/thumb_marle.png","img/thumb_lucca.png","img/thumb_frog.png","img/thumb_robo.png","img/thumb_ayla.png","img/thumb_magus.png"];
-  var imagesSpriteCharacter = ["img/char_crono.png","img/char_marle.png","img/char_lucca.png","img/char_frog.png","img/char_robo.png","img/char_ayla.png","img/char_magus.png"];
-  var canvas = document.getElementById('myCanvas');
-  var canvas2 = document.getElementById('myCanvas2');
-  var canvas3 = document.getElementById('myCanvas3');
-  var context = canvas.getContext('2d');
-  var context2 = canvas2.getContext('2d');
-  var context3 = canvas3.getContext('2d');
+  let imagesCharacter = ["img/crono.png","img/marle.png","img/lucca.png","img/frog.png","img/robo.png","img/ayla.png","img/magus.png"];
+  let imagesThumbCharacter = ["img/thumb_crono.png","img/thumb_marle.png","img/thumb_lucca.png","img/thumb_frog.png","img/thumb_robo.png","img/thumb_ayla.png","img/thumb_magus.png"];
+  let imagesSpriteCharacter = ["img/char_crono.png","img/char_marle.png","img/char_lucca.png","img/char_frog.png","img/char_robo.png","img/char_ayla.png","img/char_magus.png"];
   
+  let canvas = document.getElementById('myCanvas');
+  let canvas2 = document.getElementById('myCanvas2');
+  let canvas3 = document.getElementById('myCanvas3');
+
+  let context = canvas.getContext('2d');
+  let context2 = canvas2.getContext('2d');
+  let context3 = canvas3.getContext('2d');
+  
+  let btnGenerate = document.getElementById('generate');
+  let btnReset = document.getElementById('reset');
+
+  btnGenerate.addEventListener('click', drawingSelected);
+  btnReset.addEventListener('click', resetScreen);
+
   // drawing function
   function drawingSelected() {
-    // console.log('test');
     
-    // clear the canvas if the user press 'Generate'
-    context.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
-    context.clearRect(0, 0, 444, 125);      // clear the canvas
-    context2.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
-    context2.clearRect(0, 0, 330, 120);      // clear the canvas
-    context3.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
-    context3.clearRect(0, 0, 150, 58);      // clear the canvas
-    var selected = [];
+    // clear the canvas from preview try when user push 'Generate'
+    clearCanvas();
+    
     // check for any input:checked checkbox and push the VALUE into the array
-    $('.char__selection input:checked').each(function() {
-      selected.push($(this).attr('value'));
-      // console.log(selected);
-    });
+    let selected = document.querySelectorAll('.char__selection input:checked');
 
-    // search image inside the imagesCharacter[] and draw the image
+    // search image inside the array imagesCharacter[] and draw the image
     for (var i = 0; i < selected.length; i++) {
       var image = new Image(148,125);
       var image2 = new Image(100,100);
       var image3 = new Image(50, 58);
-      image.src = imagesCharacter[selected[i]];
-      image2.src = imagesThumbCharacter[selected[i]];
-      image3.src = imagesSpriteCharacter[selected[i]];
+      
+      image.src = imagesCharacter[selected[i].value];
+      image2.src = imagesThumbCharacter[selected[i].value];
+      image3.src = imagesSpriteCharacter[selected[i].value];
 
       //canvas 1 
       image.onload = function() {
@@ -52,12 +52,18 @@
         context3.translate(50, 0);
       }
     }
-    $('.CTchara').prop("checked", false);   // set checkbox :uncheck
   }
 
-  // clear checkbox, canvas
-  function uncheckBox(){
-    $('.CTchara').prop("checked", false);
+  // clear checkbox and canvas wh, canvas
+  function resetScreen(){
+    tempVar = document.querySelectorAll('.char__selection input:checked');
+    for(let i = 0; i < tempVar.length; i++){
+      document.querySelector('input:checked').checked = false;
+    }
+    clearCanvas();
+  }
+
+  function clearCanvas(){
     context.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
     context.clearRect(0, 0, 444, 125);      // clear the canvas
     context2.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
@@ -65,27 +71,22 @@
     context3.setTransform(1, 0, 0, 1, 0, 0); // canvas cursor position to origin
     context3.clearRect(0, 0, 150, 58);      // clear the canvas
   }
-
-  // SAVE CODE
-  // var link = document.createElement('a');
+  
+  // Save images
   const link = document.querySelector('#saveImage');
-      // link.innerHTML = 'download image';
-      link.addEventListener('click', function(ev) {
-      link.href = canvas.toDataURL();
-      link.download = "myCharacters.png";
+    link.addEventListener('click', function(ev) {
+    link.href = canvas.toDataURL();
+    link.download = "myCharacters.png";
   }, false);
-  // document.body.appendChild(link);
 
   const link2 = document.querySelector('#saveImage2');
-  // link.innerHTML = 'download image';
-  link2.addEventListener('click', function(ev) {
-  link2.href = canvas2.toDataURL();
-  link2.download = "myCharacters.png";
-}, false);
+    link2.addEventListener('click', function(ev) {
+    link2.href = canvas2.toDataURL();
+    link2.download = "myCharacters.png";
+  }, false);
 
-const link3 = document.querySelector('#saveImage3');
-// link.innerHTML = 'download image';
-link3.addEventListener('click', function(ev) {
-link3.href = canvas3.toDataURL();
-link3.download = "myCharacters.png";
-}, false);
+  const link3 = document.querySelector('#saveImage3');
+    link3.addEventListener('click', function(ev) {
+    link3.href = canvas3.toDataURL();
+    link3.download = "myCharacters.png";
+  }, false);
